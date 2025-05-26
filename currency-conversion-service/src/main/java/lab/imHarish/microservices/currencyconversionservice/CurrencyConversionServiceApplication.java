64cc10dcd172bb74f9015c0edfcfbc18c2837fa0
imHarish03 <harishhari302@gmail.com>
2025-05-26 18:40:19 +0530

    # Integrated the currency conversion service with the registry & communicated via naming server

--- currency-conversion-service/src/main/java/lab/imHarish/microservices/currencyconversionservice/CurrencyConversionServiceApplication.java
+++ currency-conversion-service/src/main/java/lab/imHarish/microservices/currencyconversionservice/CurrencyConversionServiceApplication.java
@@ -3,11 +3,12 @@ package lab.imHarish.microservices.currencyconversionservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients("lab.imHarish.microservices.currencyconversionservice")
-//@EnableCircuitBreaker
+@EnableDiscoveryClient
 public class CurrencyConversionServiceApplication {
 
 	public static void main(String[] args) {
--- currency-conversion-service/src/main/java/lab/imHarish/microservices/currencyconversionservice/CurrencyExchangeProxy.java
+++ currency-conversion-service/src/main/java/lab/imHarish/microservices/currencyconversionservice/CurrencyExchangeProxy.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 
-@FeignClient(name = "currency-exchange-service", url = "localhost:8000")
+@FeignClient(name = "currency-exchange-service")
 public interface CurrencyExchangeProxy {
 
 	@GetMapping("/currency-exchange/from/{from}/to/{to}")
